502ca72e977ef9f0d5c79c73f3bb069fcf7d6569
Bach <hi@bachhoang.com>
2016-08-06 03:23:45 +0000

    Add read write function

--- serial_write.py
+++ serial_write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import time
 import serial
-ser = serial.Serial(
+port = serial.Serial(
                port='/dev/ttyS0',
                baudrate = 9600,
                parity=serial.PARITY_NONE,
@@ -9,8 +9,11 @@ ser = serial.Serial(
                bytesize=serial.EIGHTBITS,
                timeout=1
            )
-counter=0
-while 1:
-	ser.write('Write counter: %d \n'%(counter))
-        time.sleep(1)
-        counter += 1
+raw_input('Enter your message:')
+while True: 
+#	port.write("\r\nSay something")
+#	rcv = port.read(10)
+#	port.write("\r\nYou sent:" + repr(rcv))
+	x = raw_input()
+	if( x ):
+		port.write("send:" + repr(x))
